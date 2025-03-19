ff086527e9bffe3b481622094268eb210a635e54
MadMax2121 <mmoskalenko21@gmail.com>
2025-03-19 10:55:02 -0400

    changed

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  // Adjust the basePath if your repository is named something other than your username.github.io
+  // For example, if your repo is "my-project", set basePath: '/my-project'
+  basePath: process.env.NODE_ENV === 'production' ? '/project%206%20nextjs' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
